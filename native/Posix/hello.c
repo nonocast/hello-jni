ed9c3c611a3f1a6f65bdc0c674933a9e28b57df0
nonocast <nonocast@gmail.com>
2020-04-22 11:54:10 +0800

    fix

--- native/Posix/hello.c
+++ native/Posix/hello.c
@@ -1,6 +1,7 @@
 #include "./hello.h"
 #include <stdio.h>
 
-JNIEXPORT jstring JNICALL Java_com_shgbit_App_read(JNIEnv *env, jclass cls) {
+JNIEXPORT jstring JNICALL Java_cn_nonocast_App_getContent(JNIEnv *env,
+                                                          jclass cls) {
   return (*env)->NewStringUTF(env, "hello world");
 }
