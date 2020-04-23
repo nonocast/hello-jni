034fd3c3a9deb3d8c31b7995bd277b6d1ddffdeb
nonocast <nonocast@gmail.com>
2020-04-23 12:12:06 +0800

    fix

--- misc/info/src/main/java/cn/nonocast/App.java
+++ misc/info/src/main/java/cn/nonocast/App.java
@@ -2,10 +2,10 @@ package cn.nonocast;
 
 public class App {
     public void run() {
-        System.out.println(System.getProperty("os.name"));
-        System.out.println(System.getProperty("os.version"));
-        System.out.println(System.getProperty("os.arch"));
-        System.out.println(System.getProperty("os.arch_full"));
+        System.out.println("os.name " + System.getProperty("os.name"));
+        System.out.println("os.version" + System.getProperty("os.version"));
+        System.out.println("os.arch " + System.getProperty("os.arch"));
+        System.out.println("os.arch_full " + System.getProperty("os.arch_full"));
     }
 
     public static void main(String[] args) {
